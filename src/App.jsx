f18b430a922678d73845435db4644d13763288f1
sehyeon <gs1884@naver.com>
2024-07-17 17:40:47 +0900

    로그인, 회원가입 경로 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import {Home} from "./page/Home.jsx";
 import {BoardWrite} from "./page/board/BoardWrite.jsx";
 import BoardList from "./page/board/BoardList.jsx";
+import {MemberSignup} from "./page/member/MemberSignup.jsx";
+import {MemberLogin} from "./page/member/MemberLogin.jsx";
 
 
 const router = createBrowserRouter([
@@ -11,7 +13,9 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <Home /> },
       {path:"write", element: < BoardWrite/>},
-      {path:"list", element: < BoardList/>}
+      {path:"list", element: < BoardList/>},
+      {path:"signup", element: < MemberSignup/>},
+      {path:"login", element: < MemberLogin/>},
     ]},
 ])
 
